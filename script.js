53cde31680e3c0efec74dcabcdd71fd7326daf05
Farman71 <farmanhussain71@gmail.com>
2025-02-26 12:28:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-//your JS code here. If required.
-function functionName() {
-	 alert(functionName.name);
-	
-}
-let but=document.getElementById("fun").addEventListener("onClick",functionName);		
+document.addEventListener("DOMContentLoaded", function () {
+    function functionName() {
+        alert(functionName.name); // Display function name
+    }
+
+    let button = document.getElementById("func");
+    if (button) {
+        button.addEventListener("click", functionName);
+    }
+});
